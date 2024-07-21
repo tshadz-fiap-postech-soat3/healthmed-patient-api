392262e40742bc99c347126b3e2b007e5c98e0af
tshadz <thais.sombra@hotmail.com>
2024-07-21 03:58:44 -0300

    fix: remove unused code

--- src/patients/patients.controller.ts
+++ src/patients/patients.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { CreatePatientDto } from './dto/create-patient.dto';
-import { UpdatePatientDto } from './dto/update-patient.dto';
 
 @Controller('patients')
 export class PatientsController {
@@ -23,16 +14,6 @@ export class PatientsController {
   @Get()
   findAll() {
     return this.patientsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.patientsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePatientDto: UpdatePatientDto) {
-    return this.patientsService.update(+id, updatePatientDto);
   }
 
   @Delete(':id')
